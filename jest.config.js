b825d3623e3c18f2c7e1941c4a0c5c0efdd31aef
Sergey Egorov <egorovsa2@gmail.com>
2022-01-22 20:26:12 +0200

    Update tests config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  roots: ['<rootDir>/src'],
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  roots: ["<rootDir>/src"],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/?(*.)+(spec|test).ts"],
+  globals: {
+    "ts-jest": {
+      isolatedModules: true,
+    },
+  },
 };
