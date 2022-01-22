18f45b3dbb85c909cd02f7a8e3d0ad2bb41b997e
Sergey Egorov <egorovsa2@gmail.com>
2022-01-22 20:05:16 +0200

    Reformat code

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,38 @@
 interface JsonData {
-	[key: string]: string | number | boolean;
+  [key: string]: string | number | boolean;
 }
 
 function isPathIncludesIntoLongerOne(json: JsonData, key: string) {
-	return Object.keys(json).some(
-		(keyItem) => keyItem.includes(key) && key !== keyItem
-	);
+  return Object.keys(json).some(
+    (keyItem) => keyItem.includes(key) && key !== keyItem
+  );
 }
 
 export function JsonUnFlat(json: Record<string, string | number | boolean>) {
-	const unFlatten: Object = {};
+  const unFlatten: Object = {};
 
-	Object.entries(json).forEach(([key, value]) => {
-		let branchLink: Record<string, any> = unFlatten;
+  Object.entries(json).forEach(([key, value]) => {
+    let branchLink: Record<string, any> = unFlatten;
 
-		if (isPathIncludesIntoLongerOne(json, key)) {
-			return;
-		}
+    if (isPathIncludesIntoLongerOne(json, key)) {
+      return;
+    }
 
-		const splittedKey = key.split('.');
+    const splittedKey = key.split(".");
 
-		splittedKey.forEach((splittedItem, index) => {
-			if (branchLink[splittedItem] === undefined) {
-				if (splittedKey.length - 1 === index) {
-					branchLink[splittedItem] = value;
-				} else {
-					branchLink[splittedItem] = {};
-					branchLink = branchLink[splittedItem];
-				}
-			} else {
-				branchLink = branchLink[splittedItem];
-			}
-		});
-	});
+    splittedKey.forEach((splittedItem, index) => {
+      if (branchLink[splittedItem] === undefined) {
+        if (splittedKey.length - 1 === index) {
+          branchLink[splittedItem] = value;
+        } else {
+          branchLink[splittedItem] = {};
+          branchLink = branchLink[splittedItem];
+        }
+      } else {
+        branchLink = branchLink[splittedItem];
+      }
+    });
+  });
 
-	return unFlatten;
+  return unFlatten;
 }
